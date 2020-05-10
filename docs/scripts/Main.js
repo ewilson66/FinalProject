590a20bbfcdbcf3579afc1d2bcb45e7350ff98db
ewilson66 <60370698+ewilson66@users.noreply.github.com>
2020-05-10 17:25:00 -0400

    add more things

--- docs/scripts/Main.js
+++ docs/scripts/Main.js
@@ -1,11 +1,22 @@
 $(document).ready(function()  {
 
-
-
     console.log("Yo!!");
+    $(".center").addClass("animated wobble")
 
 
-$(".center").addClass("animated wobble")
+    $(".modal-button").click(function(){
+        $(".modal").toggle().animate({
+        opacity: "1",
+        }, 1000)
+    });
+
+    $(".modal-background").click(function(){
+        $(".modal").toggle().css("opacity", "0");
+    })
+  
+    $(".modal-close").click(function(){
+        $(".modal").toggle().css("opacity", "0");
+    })
 
 });
 
