96e8f76fae9b9f2b8a4d5359dd5a94bbf48c666a
ewilson66 <60370698+ewilson66@users.noreply.github.com>
2020-05-11 13:26:21 -0400

    Even more linking
    Even more linking...

--- docs/scripts/Main.js
+++ docs/scripts/Main.js
@@ -1,4 +1,4 @@
-$(document).ready(function()  {
+$(document).ready(function(){
 
     console.log("Yo!!");
     $(".center").addClass("animated wobble")
@@ -30,8 +30,7 @@ $(document).ready(function()  {
          })
          })
 
-
-      $(document).ready(function(){
+    $(document).ready(function(){
   
         $(window).scroll(function(){
           if($(window).scrollTop() > 1500) {
@@ -40,13 +39,20 @@ $(document).ready(function()  {
          }, 1000)
            }
          })
-        
-        
-        
-      })
-
-// add javascript for sroll event here
-
+})
+        $(document).ready(function(){
+        $(".modal-button").click(function(){
+          $(".modal").toggle().animate({
+            opacity: "1",
+          }, 1000)
+        });
+        $(".modal-background").click(function(){
+          $(".modal").toggle().css("opacity", "0");
+        })
+        $(".modal-close").click(function(){
+          $(".modal").toggle().css("opacity", "0");
+        })
+    })
 
 });
 
